487e04cf3eb040a6a73599ca268f85d7fe8e3d50
Thiago Mendonça <thiagomendo.ti@gmail.com>
2024-06-16 12:49:55 -0400

    Alterando textos das paginas

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -19,4 +20,27 @@ longoBt.addEventListener('click', () => {
 function alterarContexto (contexto) {
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `../assets/img/${contexto}.png` )
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!
+            `
+            break
+        case "descanso-longo":
+            case "descanso-curto":
+            titulo.innerHTML = `
+                Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.
+            `
+            break
+        default:
+            break;
+    }
 }
