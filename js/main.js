de6f9c1298e5a6963ec96f8c3da7cafdbe1fe1a3
Thiago Mendonça <thiagomendo.ti@gmail.com>
2024-06-16 12:40:18 -0400

    Refatorando código
    Foi utilizado uma função para refatorar o codigo que estava sendo repetido

--- js/main.js
+++ js/main.js
@@ -5,17 +5,18 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '../assets/img/foco.png')
+    alterarContexto('foco')
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '../assets/img/descanso-curto.png')
+    alterarContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '../assets/img/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
 
+function alterarContexto (contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `../assets/img/${contexto}.png` )
+}
