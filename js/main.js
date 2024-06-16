aaf8e51948a9e2b408e1a4cfc04a92ce8ef30e8c
Thiago Mendonça <thiagomendo.ti@gmail.com>
2024-06-16 13:31:15 -0400

    Atualização
    Foi colocado no projeto a cor nos botões utilizando o active e logo em seguida utilizando o remove para trocar de botão

--- js/main.js
+++ js/main.js
@@ -4,20 +4,27 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto (contexto) {
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `../assets/img/${contexto}.png` )
     switch (contexto) {
@@ -39,8 +46,8 @@ function alterarContexto (contexto) {
                 Hora de voltar à superfície.<br>
                 <strong class="app__title-strong">Faça uma pausa longa.
             `
-            break
         default:
             break;
     }
 }
+
