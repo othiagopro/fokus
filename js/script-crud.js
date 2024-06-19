970558028ad8e13ba603d625d026a33850b48de3
Thiago Mendonça <thiagomendo.ti@gmail.com>
2024-06-18 22:00:20 -0400

    Atualizando codigo

---
+++ js/script-crud.js
@@ -0,0 +1,9 @@
+// encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden')
+})
